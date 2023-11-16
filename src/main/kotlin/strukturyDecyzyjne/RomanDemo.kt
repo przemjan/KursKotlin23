203b70b0933f15c909f2870dcff38fc3ef4565c3
Przemek Janisz <przemjan@przemjan.pl>
2023-11-16 18:28:50 +0100

    Dodanie roman rozwiązanie2

--- src/main/kotlin/strukturyDecyzyjne/RomanDemo.kt
+++ src/main/kotlin/strukturyDecyzyjne/RomanDemo.kt
@@ -1,24 +1,41 @@
 package Zrobione.decyzje.whenDemos
 
+import kotlin.system.exitProcess
+
 fun main() {
 
-    val liczba = 17
+    val lowerLimit = 1
+    val upperLimit = 10
 
-    var flag:Boolean = true
+    val welcomeMessage = "Podaj liczbę od $lowerLimit do $upperLimit"
 
-    val output = when(liczba){
-        9->"IX"
+    println(welcomeMessage)
+
+    val liczba = readln().toInt()
+    val errorMessage = "Liczba nie mieści się w zakresie $lowerLimit do $upperLimit"
+
+    if(liczba !in lowerLimit .. upperLimit){
+        println(errorMessage)
+        exitProcess(0)
+    }
+
+    val roman = when(liczba){
+        1-> "I"
+        2-> "II"
+        3-> "III"
+        4-> "IV"
+        5-> "V"
+        6-> "VI"
+        7-> "VII"
+        8-> "VIII"
+        9-> "IX"
         10->"X"
         else->{
-            flag = false
-            "Nie ma takiego numeru"
+            liczba
         }
     }
 
-    if(flag){
-        println("Rzymska to: $output")
-    }
-    else{
-        println(output)
-    }
+    val resultMessage = "Liczba $liczba to w systemie rzymskim: $roman"
+    println(resultMessage)
+
 }
